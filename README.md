4e6e2dfdd5ba0c753abe3c5de3d7353455931445
Junior-Stranner <Ju-stranner@outlook.com>
2024-04-14 17:34:26 -0300

    page created Graphical interface created

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Pong-Game-Java
+
+
+Graphical interface created
+
+![Interface gráfica criada.png](Interface%20gr%E1fica%20criada.png)
--- src/main/java/br/com/judev/ponggame/GamePanel.java
+++ src/main/java/br/com/judev/ponggame/GamePanel.java
@@ -28,6 +28,12 @@ public class GamePanel extends JPanel implements Runnable{
         newPaddles();
         newBall();
         score = new Score(GAME_WIDTH, GAME_HEIGHT);
+        this.setFocusable(true);
+        this.addKeyListener(new AL());
+        this.setPreferredSize(SCREEN_SIZE);
+
+        gameThread = new Thread(this);
+        gameThread.start();
     }
 
     public void newBall(){
